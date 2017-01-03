07bd3af1d1479d3f22d94a296454a0918a6edf6b
Makoto Ikeda <mike.app.info@gmail.com>
2017-01-03 15:39:45 +0900

    静的コンテンツ表示

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-LetsEncrypt"
+    name: "Kitura-LetsEncrypt",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Kitura
+import HeliumLogger
+
+// Initialize HeliumLogger
+HeliumLogger.use()
+
+// Create a new router
+let router = Router()
+router.all(middleware: StaticFileServer())
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
