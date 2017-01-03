eac528e6445fef68db09ca2127cd3e3edd71dd10
Makoto Ikeda <mike.app.info@gmail.com>
2017-01-03 16:32:36 +0900

    SSL対応
    Let's encrypt専用

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,28 @@
 import Kitura
 import HeliumLogger
 
+// TODO: change domain
+let domain = "example.com"
+
 // Initialize HeliumLogger
 HeliumLogger.use()
+
+// Initialize SSL
+let caPath = "/etc/letsencrypt/live/\(domain)/"
+let certPath = "/etc/letsencrypt/live/\(domain)/cert.pem"
+let keyPath = "/etc/letsencrypt/live/\(domain)/privkey.pem"
+let sslConfig = SSLConfig(withCACertificateDirectory: caPath,
+                          usingCertificateFile: certPath,
+                          withKeyFile: keyPath,
+                          usingSelfSignedCerts: false)
 
 // Create a new router
 let router = Router()
 router.all(middleware: StaticFileServer())
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.addHTTPServer(onPort: 80, with: router)
+Kitura.addHTTPServer(onPort: 443, with: router, withSSL: sslConfig)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
